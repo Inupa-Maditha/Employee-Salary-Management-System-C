d98d2913cc05d2a54dde59ccfcdc71603934e71b
InupaMaditha <inupa.build@yahoo.com>
2026-03-26 17:18:56 +0530

    Add extern declarations and move struct definition to functions.h

--- functions.h
+++ functions.h
@@ -1,4 +1,13 @@
-void addData(struct employees EMP[],int size);
+extern int loopCount;
+extern int option;
+extern int size;
+struct employees{
+    int id;
+    char name[20];
+    int salaryBasic;
+    float salaryGross;
+};
+void addData(struct employees EMP[],int count);
 void editRow(struct employees EMP[]);
 void deleteRaw(struct employees EMP[]);
 void previewChanges(struct employees EMP[]);
