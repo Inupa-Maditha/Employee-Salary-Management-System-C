255de881cd5e219dbe11427970b86437e9cdd8d8
InupaMaditha <inupa.build@yahoo.com>
2026-03-26 17:14:54 +0530

    Fix multiple bugs and improve output formatting

--- functions.c
+++ functions.c
@@ -1,54 +1,73 @@
 #include <stdio.h>
+#include <string.h>
+#include "functions.h"
 
-void addData(struct employees EMP[],int size){
+#define TABLE_WIDTH 69
+
+void addData(struct employees EMP[],int count){
 
     printf("Enter Employee ID, Employee Name, Basic Salary :\n");
-    while(loopCount<size){
-        printf("(%03d) : ",loopCount+1);
+    for(int i=0; i<count; i++){
+        printf("%02d : ",loopCount+1);
         getEmployeeDetails(EMP,loopCount);
         loopCount++;
     }
-    menu();
+    printf("\n");
 }
 
 void editRow(struct employees EMP[]){
 
     int num;
-    previewChanges();
+    previewChanges(EMP);
     printf("Enter Row Number You Want Edit : ");
     scanf("%d",&num);
-    printf("Enter Employee ID, Employee Name, Basic Salary : ");
-    getEmployeeDetails(EMP,num-1);
-    menu();
+    if(num>0 && (num-1)<loopCount){
+        printf("Enter Employee ID, Employee Name, Basic Salary : ");
+        getEmployeeDetails(EMP,num-1);
+    }else{
+        printf("Invalid input\n\n");
+    }
 }
 
 void deleteRaw(struct employees EMP[]){
 
     int num;
-    previewChanges();
+    previewChanges(EMP);
     printf("Enter Row Number You Want Delete : ");
     scanf("%d",&num);
-    for(int i=num-1;i<loopCount;i++){
-        EMP[i].id = EMP[i+1].id;
-        EMP[i].name = EMP[i+1].name;
-        EMP[i].salaryBasic = EMP[i+1].salaryBasic;
-        EMP[i].salaryGross = EMP[i+1].salaryGross;
+    if(num>0 && (num-1)<loopCount){
+        for(int i=num-1;i<loopCount;i++){
+            EMP[i].id = EMP[i+1].id;
+            strcpy(EMP[i].name,EMP[i+1].name);
+            EMP[i].salaryBasic = EMP[i+1].salaryBasic;
+            EMP[i].salaryGross = EMP[i+1].salaryGross;
+        }
+        EMP[loopCount].id = 0;
+        EMP[loopCount].name[0] = '\0';
+        EMP[loopCount].salaryBasic = 0;
+        EMP[loopCount].salaryGross = 0.0;
+        loopCount--; 
+    }else{
+        printf("Invalid input\n\n");
     }
-    EMP[loopCount].id = 0;
-    EMP[loopCount].name[0] = '\0';
-    EMP[loopCount].salaryBasic = 0;
-    EMP[loopCount].salaryGross = 0.0;
-    loopCount--;
-    menu();
 }
 
 void previewChanges(struct employees EMP[]){
-
-    printf("| %-02s| %-5s| %-20s| %-15s| %-15s|\n","NO:","ID","Employee Name","Basic Salary","Gross Salary");
-    for(int i=0;i<loopCount;i++){
-        printf("| %-02d| %-5d| %-20s| %-15d| %-15.2f|\n",loopCount+1,EMP[loopCount].id,EMP[loopCount].name,EMP[loopCount].salaryBasic,EMP[loopCount].salaryGross);
+    for(int i=0;i<TABLE_WIDTH;i++){
+        printf("-");
     }
-    menu();
+    printf("\n| %3s| %-5s| %-20s| %-15s| %-15s|\n","NO:","ID","Employee Name","Basic Salary","Gross Salary");
+    for(int i=0;i<TABLE_WIDTH;i++){
+        printf("-");
+    }
+    printf("\n");
+    for(int i=0;i<loopCount;i++){
+        printf("| %02d | %-5d| %-20s| %-15d| %-15.2f|\n",i+1,EMP[i].id,EMP[i].name,EMP[i].salaryBasic,EMP[i].salaryGross);
+    }
+    for(int i=0;i<TABLE_WIDTH;i++){
+        printf("-");
+    }
+    printf("\n");
 }
 
 void saveChanges(struct employees EMP[]){
@@ -56,24 +75,22 @@ void saveChanges(struct employees EMP[]){
     FILE *fp;
     fp = fopen("dataSheet.txt","a");
     for(int i=0;i<loopCount;i++){
-        fprintf("%15s : %-02d\n%15s : %-5d\n%15s : %-20s\n%15s : %-15d\n%15s : %-15.2f\n\n","Employee ID",EMP[i].id,"Employee Name",EMP[i].name,"Basic Salary",EMP[i].salaryBasic,"Gross Salary",EMP[i].salaryGross);
+        fprintf(fp,"%-15s : %-5d\n%-15s : %-20s\n%-15s : %-15d\n%-15s : %-15.2f\n\n","Employee ID",EMP[i].id,"Employee Name",EMP[i].name,"Basic Salary",EMP[i].salaryBasic,"Gross Salary",EMP[i].salaryGross);
     }
     fclose(fp);
     loopCount = 0;
     size = 0;
-    menu();
+    printf("Your changes saved..\n");
 }
 
 void viewDataTable(){
-    menu();
 }
 
 void editDataTable(){
-    menu();
 }
 
 void menu(){
-    printf("(01) %15s : 1\n(02) %15s : 2\n(03) %15s : 3\n(04) %15s : 4\n(05) %15s : 5\n(06) %15s : 6\n(07) %15s : 7\n(08) %15s : 8\n\nEnter Your Option : ","Add Data","Edit Raw","Delete Raw","Preview Changes","Save Changes","View Data Table","Edit Data Table","Exit");
+    printf("\n(01) %-15s : 1\n(02) %-15s : 2\n(03) %-15s : 3\n(04) %-15s : 4\n(05) %-15s : 5\n(06) %-15s : 6\n(07) %-15s : 7\n(08) %-15s : 8\n\nEnter Your Option : ","Add Data","Edit Raw","Delete Raw","Preview Changes","Save Changes","View Data Table","Edit Data Table","Exit");
     scanf("%d",&option);
     printf("\n");
 }
@@ -88,6 +105,6 @@ void getEmployeeDetails(struct employees EMP[],int num){
 float calculateSalaryGross(int basicSalary){
     
     float grossSalary,transportAllowance = (float)basicSalary*10/100, attendanceAllowance = (basicSalary>35000)? (float)basicSalary*5/100:(float)basicSalary*8/100;
-    grossSalary = transportAllowance + attendanceAllowance;
+    grossSalary = transportAllowance + attendanceAllowance + basicSalary;
     return grossSalary;
 }
